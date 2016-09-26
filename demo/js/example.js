86b10c011ca2739d94e499ccf43c708b82790faa
Gaurav Kumar <aavrug@gmail.com>
2016-09-26 16:47:05 +0530

    Updated demo js.

--- demo/js/example.js
+++ demo/js/example.js
@@ -1,4 +1,9 @@
 $(document).ready( function() {
-	var savedBoxes = [{"id":"square-306-238","left":"306","top":"238","width":"40","height":"27"}, {"id":"square-660-312","left":"660","top":"312","width":"80","height":"51"}, {"id":"square-380-265","left":"380","top":"265","width":"180","height":"124"}];
-    $('#scroom-block').scrollZoom({boxes: savedBoxes});
+	var savedBoxes = [
+        {"id":"square-306-238","left":"306","top":"238","width":"40","height":"27", "name":"test1", "description":"testdes1"},
+        {"id":"square-660-312","left":"660","top":"312","width":"80","height":"51", "name":"test2", "description":"testdes2"},
+        {"id":"square-380-265","left":"380","top":"265","width":"180","height":"124", "name":"test3", "description":"testdes3"}
+    ];
+	var displayFields = ['name', 'description'];
+    $('#scroom-block').scrollZoom({boxes: savedBoxes, hoverData: displayFields});
 });
